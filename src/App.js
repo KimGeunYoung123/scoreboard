258f76d02500d2e0bc9e093e0918ecc6fa2b7b3c
KimGeunYoung123 <kky-jjang@hanmail.net>
2020-01-18 17:07:24 +0900

    리팩토링

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './components/Header';
+import {Player} from './components/Player';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    players: [
+      {name: 'LDK', id: 1},
+      {name: 'HONG', id: 2},
+      {name: 'KIM', id: 3},
+      {name: 'PARK', id: 4}
+    ]
+  }
+
+  handRemovePlayer = (id) => {
+    console.log('handleRemove : ' + id);
+    // 해당되는 id를 삭제
+    this.setState(prevState => {
+      const players = prevState.players.filter(item => item.id !== id);
+      // es6 short hand property: 키와 값이 같으면 한쪽 생략
+      //return { players: players}
+      return { players}
+    })
+  }
+
+  render() {
+    return (
+        <div className="scroeboard">
+          <Header title="My Scoreboard" totalPlayers={11} />
+          {
+            //props.initialPlayers.map(item => <Player name={item.name} score={item.score} key={item.id.toString()} />)
+            //props.initialPlayers.map(item => <Player name={item.name} key={item.id.toString()}/>)
+            this.state.players.map(item => <Player name={item.name} key={item.id.toString()} removePlayer={this.handRemovePlayer} id={item.id} />)
+          }
+        </div>
+    )
+  }
 }
 
 export default App;
